423a39dba4b35c8149e4ef6d94e60a453f7a6f1e
prince70 <942892084@qq.com>
2019-04-22 16:43:54 +0800

    Initial commit

---
+++ app/src/main/java/com/niwj/airecognition/MainActivity.java
@@ -0,0 +1,19 @@
+package com.niwj.airecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+}
